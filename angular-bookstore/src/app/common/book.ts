f1a153d736949b5e6059931d3f0b4788a973bdf9
subhashrsrivatsa <subhashrsrivatsa.paris@atlassian.com>
2020-12-11 23:12:37 +0100

    created a html table, array of books(hardcoded)

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,14 @@
+export class Book {
+
+    // same fields as that of the SpringBoot Book class
+
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageURL: string;
+    active: boolean;
+    unitsInStock: number;
+    createdOn: Date;
+    updatedOn: Date;
+}
